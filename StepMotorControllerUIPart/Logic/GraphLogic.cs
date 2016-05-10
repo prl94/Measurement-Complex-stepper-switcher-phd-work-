bfdb06e68d1448418f02f3efb515c13bac0c5909
Volodymyr Parlah <vprl1994@gmail.com>
2016-05-11 00:49:59 +0300

    in progress Logic/GraphLogic.cs

---
+++ StepMotorControllerUIPart/Logic/GraphLogic.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StepMotorControllerUIPart.UsedTypes;
+using ZedGraph;
+
+namespace StepMotorControllerUIPart.Logic
+{
+    static class GraphLogic
+    {
+        
+
+        public static PointPairList PrepareDataForGraph(List<MesureLatest> mesuresList)
+        {
+            int count = mesuresList.Count;
+
+            double[] InormArray = new double[count];
+            float[] DiaphragmasArray = new float[count];
+
+            for (int i = 0; i < count; i++)
+            {
+              InormArray[0] = mesuresList[0].Inorm;
+              DiaphragmasArray[0] =  mesuresList[0].Diaphragm;
+            }
+
+            double[] InormArrayComplete = new double[count];
+            float[] DiaphragmasArrayComplete = new float[count];
+
+
+            //todo logic for graph
+            for (int i = 0; i < count; i++)
+            {
+
+            }
+
+
+
+            // diaphragmas array
+            DiaphragmasArrayComplete[0] = 0;
+            float val = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (i == count-1)
+                {
+                    break;
+                }
+                val += DiaphragmasArray[i];
+                DiaphragmasArrayComplete[i + 1] = val;
+
+            }
+
+
+
+
+            return null;
+        }
+    }
+}
---
+++ StepMotorControllerUIPart/UsedTypes/MesureLatest.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using StepMotorControllerUIPart.Helper;
+
+namespace StepMotorControllerUIPart.UsedTypes
+{
+    public class MesureLatest
+    {
+
+        public readonly int MesureNumber;
+        public readonly float[] USecondaryEmitionMonitor;
+        public readonly float[] UChannel;
+        public float Resistor;
+        public float Diaphragm;
+
+        public double Inorm {
+            get { return (USecondaryEmitionMonitorAverage / Resistor) / UChannelAverage; }
+        }
+    
+        
+
+        //average
+        public double USecondaryEmitionMonitorAverage {
+            get { return USecondaryEmitionMonitor.Average(); }
+        }
+        public double UChannelAverage
+        {
+            get { return UChannel.Average(); }
+        }
+
+        //StandardDeviation
+        public double USecondaryEmitionMonitorStandardDeviation
+        {
+            get { return MathHelper.GetStandardDeviation(USecondaryEmitionMonitor); }
+        }
+
+        public MesureLatest(int mesureNumber, float[] uSecondaryEmitionMonitor, float[] uChannel, float resistor, float diaphragm)
+        {
+            MesureNumber = mesureNumber;
+            USecondaryEmitionMonitor = uSecondaryEmitionMonitor;
+            UChannel = uChannel;
+            Resistor = resistor;
+            Diaphragm = diaphragm;
+        }
+        
+
+    }
+}
